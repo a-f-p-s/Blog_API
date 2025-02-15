9719e85b11fee88275e7baf1e5a9f51395fed889
Andy <felipeandresps02@gmail.com>
2025-02-14 20:22:11 -0500

    dando inicio al server de express

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
-console.log("Start project");
+const http = require('http')
+const fs = require ('fs')
+
+const server = http.createServer((req, res)=> {
+    const read = fs.createReadStream('./static/index.html')
+    read.pipe(res)
+})
+
+server.listen(3000)
