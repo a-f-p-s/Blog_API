c213d4b8a1ec945ff72a9c0cf7682fcda33b6807
Andy <felipeandresps02@gmail.com>
2025-02-14 21:32:02 -0500

    testeando funcionalidades de express

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-const http = require('http')
-const fs = require ('fs')
+const express = require('express')
 
-const server = http.createServer((req, res)=> {
-    const read = fs.createReadStream('./static/index.html')
-    read.pipe(res)
-})
+const server = express()
+
+server.get('/', (req, res) => {
+    let welcome = "Welcome user to Andres PS API"
+    res.send(welcome)
+}) 
+server.get('/post', (req, res) => {
+    data = res.sendFile('./static/books.json', {
+        root: __dirname
+    })
+}) 
+server.post('/post/:Book', (req, res) => {
+    let titulo = 'la gran noticia del año'
+    res.send('deseas crear un post titulado :' + req.params)
+}) 
+server.get('/user', (req, res) => {
+      
+    data = res.sendFile('./static/index.json', {
+        root: __dirname
+    })
+
+}) 
+server.get('/coment', (req, res) => {
+    res.send('Hello World')
+}) 
+server.get('/post/all', (req, res) => {
+    res.send('Hello World')
+}) 
+server.get('/user/all', (req, res) => {
+    res.send('Hello World')
+}) 
+server.get('/coment/all', (req, res) => {
+    res.send('Hello World')
+}) 
 
 server.listen(3000)
